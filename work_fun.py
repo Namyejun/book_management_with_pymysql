ccf07636a6aea398834c504522133cb1dcbd8b37
Namyejun <yejun1148@gmail.com>
2023-03-15 17:58:42 +0900

    debug work_fun

--- work_fun.py
+++ work_fun.py
@@ -114,4 +114,4 @@ def quit(a): #quit
     print("#########################")
     exit()
 
-work_menu = [quit, fun1, fun2, fun3, fun4, fun5, fun6]
+work_menu = [quit, fun1, fun2, fun3, fun4, fun5, fun6, fun7]
